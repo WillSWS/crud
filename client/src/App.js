e8644c2ef84d9b63137a3d2025cc1177786623fe
Samuel <samuelwill.ds@gmail.com>
2022-05-15 17:43:37 -0300

    nova tentativa

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div classname="Information">
+        <label>Name:</label>
+        <input type="text" />
+        <label>Age:</label>
+        <input type="number" />
+        <label>Country:</label>
+        <input type="text" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Wage (year):</label>
+        <input type="number" />
+      </div>
+    </div>
   );
 }
 
