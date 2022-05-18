79c8ce60717013620b24e41edee4949b4810c88a
Samuel <samuelwill.ds@gmail.com>
2022-05-18 14:25:23 -0300

    início da implantação do back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,57 @@
-import "./App.css"
-import {useState} from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  const [Name, setName] = useState('');
+  const [Name, setName] = useState("");
   const [Age, setAge] = useState(0);
-  const [Country, setCountry] = useState('');
-  const [Position, setPosition] = useState('');
+  const [Country, setCountry] = useState("");
+  const [Position, setPosition] = useState("");
   const [Wage, setWage] = useState(0);
+
+ 
   return (
     <div className="App">
       <div className="information">
         <label>Name:</label>
         <input
-         type="text"
+          type="text"
           onChange={(event) => {
             setName(event.target.value);
           }}
         />
         <label>Age:</label>
-        <input type="number" />
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
         <label>Country:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
         <label>Position:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
         <label>Wage(year):</label>
-        <input type="number" />
-        <button>Add Employee</button>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button >Add Employee</button>
       </div>
     </div>
   );
 }
 export default App;
+
+// <button onClick={displayInfo}>Add Employee</button>
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express ()
+
+
+
+app.listen(3001, () => {
+    console.log("Hey, your server is running in on port 3001")
+})
